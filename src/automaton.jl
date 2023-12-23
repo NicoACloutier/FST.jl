50afb4586a44c6c794f5037cba40968cb473f708
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2023-12-23 16:43:36 -0500

    reorganize automaton code

--- src/automaton.jl
+++ src/automaton.jl
@@ -1,74 +1,54 @@
 """
-A node of a finite-state automaton.
+An arc between two nodes in a finite-state machine.
 """
-struct Node
-    pattern::String                    # the string pattern to match on the node
-    next::Vector{Union{Node, Nothing}} # a vector of nodes it points to, `::Nothing` if this is a terminal node.
+struct Arc
+    origin::Int      # the origin node
+    destination::Int # the destination node
+    pattern::String  # the arc pattern
 end
 
 """
-A finite-state automaton. The first node should contain an empty string and point to the real first node.
+A finite-state automaton.
 """
 struct FSAutomaton
-    starter::Node # the first node
+    starter::Int      # the first node
+    final::Int        # the final node
+    arcs::Vector{Arc} # connections between nodes
 end
 
 """
-Find a pattern match in a series of next nodes given a node to check on.
-Arguments:
-    `pattern::String`: the pattern.
-    `node::Node`: the node to check the next nodes of.
-Returns:
-    `::Union{Int, Nothing, Missing}`: `::Int` with the index of the first matching pattern if present, `::Nothing` if terminal node, 
-        `::Missing` if no node was found with a matching pattern.
+Find a vector of integer IDs for possible next nodes given a node and a finite-state automaton.
 """
-function find_pattern(pattern::String, node::Node)::Union{Int, Nothing, Missing}
-    for (i, potential_next) in enumerate(node.next)
-        if isnothing(potential_next)
-            return nothing
-        elseif pattern == potential_next.pattern
-            return i
-        end
-    end
-    return missing
+function next(machine::FSAutomaton, node::Int)::Vector{Arc}
+    filter(arc -> arc.origin == node, machine.arcs)
 end
 
 """
-Recognize a string pattern utilizing a fine-state automaton.
-Arguments:
-    `patterns::Vector{String}`: the patterns to be recognized by the finite-state automaton.
-    `machine::FSAutomaton`: the finite-state automaton.
-Returns:
-    `::Bool`: whether the string pattern is matched.
+Recognize a vector of string patterns utilizing a finite-state automaton, return whether recognized.
 """
 function recognize(patterns::Vector{String}, machine::FSAutomaton)::Bool
     current_node = machine.starter
-    for (i, pattern) in enumerate(patterns)
-        next = find_pattern(pattern, current_node)
-        if isnothing(next) && i != length(patterns)
-            return false
-        elseif ismissing(next)
+    for (i, pattern) in enumerate(patterns[1:end-1])
+        potentials = filter(arc -> arc.pattern == pattern, next(machine, current_node))
+        if length(potentials) == 0
             return false
         end
-        current_node = current_node.next[next]
+        current_node = potentials[1].destination
     end
-    map(isnothing, current_node.next) |> any
+    any(arc -> arc.pattern == patterns[end] && arc.destination == machine.final, next(machine, current_node))
 end
 
 """
-Generate a string pattern utilizing a finite-state automaton. Automatically selects the first next node if multiple are present.
-    Not guaranteed to reach a stopping point.
-Arguments:
-    `machine::FSAutomaton`: the finite-state automaton.
-Returns:
-    `::Vector{String}`: the output.
+Generate a string pattern using a finite-state automaton.
+    Not guaranteed to stop.
 """
 function generate(machine::FSAutomaton)::Vector{String}
     pattern::Vector{String} = []
     current_node = machine.starter
-    while !isnothing(current_node)
-        push!(pattern, current_node.pattern)
-        current_node = current_node.next[1]
+    while current_node != machine.final
+        arc = next(machine, current_node)[1]
+        push!(pattern, arc.pattern)
+        current_node = arc.destination
     end
-    pattern[2:end]
+    pattern
 end
